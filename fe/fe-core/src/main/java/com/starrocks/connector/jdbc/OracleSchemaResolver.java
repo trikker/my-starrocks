77befe98b69d73d4c31649cf3ea5525999e14742
yangdongchao <yangdongchao@corp.netease.com>
2024-02-26 20:43:30 +0800

    add oracle partition information fix 3

--- fe/fe-core/src/main/java/com/starrocks/connector/jdbc/OracleSchemaResolver.java
+++ fe/fe-core/src/main/java/com/starrocks/connector/jdbc/OracleSchemaResolver.java
@@ -32,6 +32,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Types;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
