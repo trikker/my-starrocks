192b0cd4553ee26e5e146fad0a76b633ee438483
yangdongchao <yangdongchao@corp.netease.com>
2024-02-27 14:44:55 +0800

    fix

--- fe/fe-core/src/main/java/com/starrocks/connector/jdbc/OracleSchemaResolver.java
+++ fe/fe-core/src/main/java/com/starrocks/connector/jdbc/OracleSchemaResolver.java
@@ -225,7 +225,7 @@ public class OracleSchemaResolver extends JDBCSchemaResolver {
     @Override
     public List<String> listPartitionNames(Connection connection, String databaseName, String tableName) {
         String partitionNamesQuery =
-                "SELECT p.PARTITION_NAME AS NAME" +
+                "SELECT p.PARTITION_NAME AS NAME " +
                 "FROM ALL_TAB_PARTITIONS p " +
                 "    join ALL_PART_TABLES t " +
                 "    ON p.TABLE_OWNER = t.OWNER " +
